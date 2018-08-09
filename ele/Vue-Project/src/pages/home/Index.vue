ab4e0d27a769a43916114de3bb9ed6099ffda5fe
wdn <269693659@qq.com>
2018-08-09 09:16:49 +0800

    home stop 完成

--- ele/Vue-Project/src/pages/home/Index.vue
+++ ele/Vue-Project/src/pages/home/Index.vue
@@ -1,5 +1,5 @@
 <template>
-    <page id="home">
+    <page id="home" :onScroll="getScrollY" ref="page">
         <div class="main">
             <div class="search">
                 <div class="aBox"><p>搜索饿了么商家、商品的名称</p></div>
@@ -10,10 +10,10 @@
             <!-- 排序 -->
             <Fox></Fox>
             <!-- 推荐商家 -->
-            <Stops></Stops>
-            <Stops></Stops>
-            <Stops></Stops>
-            <Stops></Stops>
+            <div class="stops">
+                <Stops v-for="(item,i) in stopsData" :key="i" :data="item"></Stops>
+            </div>
+            
         </div>
     </page>
 </template>
@@ -22,7 +22,9 @@
 import Swiper from '@/components/home/Swiper.vue'
 import Stops from '@/components/common/stops'
 import Fox from '@/components/home/fox'
+//数据
 import {getHomeBannerData} from '@/services/bannerService.js'
+import { getSaleList } from "../../services/discoverserver";
 import {
   getRestaurantLocation,
   getAddressLocation,
@@ -30,12 +32,16 @@ import {
   getAttributeLocation,
   getSortList
 } from "../../services/indexserver";
-import { getSaleList } from "../../services/discoverserver";
 
 export default {
     data(){
        return {
-           bannerData:[]
+            bannerData:[],
+            stopsData:[],
+            offset:0,
+            limit:8,
+            getAlllimit:0,
+            canGetLocation:true,
        }
     },
     components:{
@@ -43,33 +49,58 @@ export default {
         Stops,
         Fox
     },
- 
+    methods:{
+        getScrollY(y){
+            if(y<20 && this.canGetLocation){
+                this.requestRestaurantLocation()
+            }
+        },
+        //再次请求数据
+        requestRestaurantLocation(){
+            this.canGetLocation = false;
+            getRestaurantLocation(this.getAlllimit,this.limit).then(result => {
+                this.stopsData = this.stopsData.concat(result)
+                this.$nextTick(()=>{
+                   
+                    this.$refs.page.refreshDOM()
+                    this.getAlllimit += this.limit
+                    this.canGetLocation = true;
+                })
+                
+            })
+        }
+
+    },
     mounted() {
-    //请求轮播图的数据
 
-    getHomeBannerData().then(result=>{
-
+        //请求轮播图的数据
+        getHomeBannerData().then(result=>{
         this.bannerData=result;
         // console.log(this.bannerData)
-    })
+        })
 
-    // 请求轮播图的数据
-    getRestaurantLocation().then(result => {
-    //   console.log(result, "饭店信息");
-    });
-    getAddressLocation().then(result => {
-    //   console.log(result, "本地地址");
-    });
-    getAttributeLocation().then(result => {
-      console.log(result, "小属性");
-    });
-    getSortList().then(result => {
-      console.log(result, "排序");
-    });
-    getSaleList().then(result => {
-    //   console.log(result, "美食");
-    });
-  }
+        // 请求商家的数据
+        getRestaurantLocation(this.offset,this.limit).then(result => {
+            this.stopsData = result;
+            this.getAlllimit += this.limit
+            console.log(this.getAlllimit)
+        })
+
+
+        // getAddressLocation().then(result => {
+        // //   console.log(result, "本地地址");
+        // });
+        // getAttributeLocation().then(result => {
+        // console.log(result, "小属性");
+        // });
+        // getSortList().then(result => {
+        // console.log(result, "排序");
+        // });
+        // getSaleList().then(result => {
+        // //   console.log(result, "美食");
+
+        // });
+    }
 }
 </script>
 
