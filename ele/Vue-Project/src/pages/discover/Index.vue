fe9bf2061346f92ad34b620443ac2c2a864a79f1
chenjiajunxi <chenjiajunxi>
2018-08-09 09:14:00 +0800

    777777

--- ele/Vue-Project/src/pages/discover/Index.vue
+++ ele/Vue-Project/src/pages/discover/Index.vue
@@ -53,7 +53,7 @@
                     </li>
                 </ul>
                 <div class="more">
-                    <p>更多</p>
+                    <p>查看更多</p>
                 </div>
             </div>
         </div>
@@ -149,7 +149,7 @@ export default {
     }
     .shopList ul{
         width:100%;
-        height:387px;
+        height:300px;
         display: flex;
         margin-top:25px;
     }
@@ -201,5 +201,10 @@ export default {
            position: absolute;
            top:350px;
            left:120px;
+    }
+    .more p{
+        margin-top:-98px;
+        margin-left:156px;
+        color:#ccc;
     }
 </style>
