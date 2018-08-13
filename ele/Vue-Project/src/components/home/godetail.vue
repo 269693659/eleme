d3c73921cf80ee0bcb57b0fe09f97145a250890e
xiaoli <424465344@qq.com>
2018-08-13 12:05:53 +0800

    合并前缓存在自分支

--- ele/Vue-Project/src/components/home/godetail.vue
+++ ele/Vue-Project/src/components/home/godetail.vue
@@ -17,161 +17,52 @@
  愿您尝遍世间百态转身依旧爱恋一号粥店✌</p>
         </div>
         <ul class="addFoodList flex">
-          <li><a>点餐</a></li>
-          <li><a>评价</a></li>
-          <li><a>商家</a></li>
+          <li @click="Ordering(1)"><a>点餐</a></li>
+          <li @click="Ordering(2)"><a>评价</a></li>
+          <li @click="Ordering(3)"><a>商家</a></li>
         </ul>
-        <h5 class="storeito">商家推荐</h5>
-        <ul class="swiperList flex">
-          <li>
-            <img src="//fuss10.elemecdn.com/6/86/01eb17f3fe4313a255bc9a1eefb54jpeg.jpeg?imageMogr/format/webp/thumbnail/240x/" alt="">
-            <p class="textell fds1">香菇瘦肉粥+炸饺+牛肉饼+网红鲜椒酱</p>
-            <p class="monsale"> 月售82好评率100%</p>
-            <p class="price"><small>¥</small>16.74</p>
-            <span  class="addBlue">+</span>
-          </li>
-          <li>
-            <img src="//fuss10.elemecdn.com/1/0a/623d1d0900454feb93c5f01aa4a57jpeg.jpeg?imageMogr/format/webp/thumbnail/240x/" alt="">
-            <p class="textell fds1">香菇瘦肉粥+炸饺+牛肉饼+网红鲜椒酱</p>
-            <p class="monsale"> 月售82好评率100%</p>
-            <p class="price"><small>¥</small>16.74</p>
-            <span class="addBlue">+</span>
-          </li>
-          <li>
-            <img src="//fuss10.elemecdn.com/d/7b/8f1ae2bab7f64c3ae33b947ec8a7djpeg.jpeg?imageMogr/format/webp/thumbnail/240x/" alt="">
-            <p class="textell fds1">香菇瘦肉粥+炸饺+牛肉饼+网红鲜椒酱</p>
-            <p class="monsale"> 月售82好评率100%</p>
-            <p class="price"><small>¥</small>16.74</p>
-            <span  class="addBlue">+</span>
-          </li>
-          <li>
-            <img src="//fuss10.elemecdn.com/c/ad/8d5de0a5338d517048c7241449c64jpeg.jpeg?imageMogr/format/webp/thumbnail/240x/" alt="">
-            <p class="textell fds1">香菇瘦肉粥+炸饺+牛肉饼+网红鲜椒酱</p>
-            <p class="monsale"> 月售82好评率100%</p>
-            <p class="price"><small>¥</small>16.74</p>
-            <span  class="addBlue">+</span>
-          </li>
-          <li>
-            <img src="//fuss10.elemecdn.com/c/ad/8d5de0a5338d517048c7241449c64jpeg.jpeg?imageMogr/format/webp/thumbnail/240x/" alt="">
-          </li>
-          <li>
-            <img src="//fuss10.elemecdn.com/a/2c/30d111bd1f8b520ac517f69b67e62jpeg.jpeg?imageMogr/format/webp/thumbnail/240x/" alt="">
-          </li>
-          <li>
-            <img src="//fuss10.elemecdn.com/7/00/27d39196ba86b1e153881eb0e0949jpeg.jpeg?imageMogr/format/webp/thumbnail/240x/" alt="">
-          </li>
-          <li>
-            <img src="//fuss10.elemecdn.com/c/49/da1dd70d0e363e0d4a775f168d0b7jpeg.jpeg?imageMogr/format/webp/thumbnail/240x/" alt="">
-          </li>
-        </ul>
-        <div class="detailBox">
-            <div class="dBoxLeft">
-                <ul>
-                  <li>热销</li>
-                  <li>优惠</li>
-                  <li>店铺优惠</li>
-                  <li>每周新品</li>
-                  <li>清热白粥</li>
-                  <li>生滚咸粥</li>
-                  <li>包子馒头</li>
-                  <li>特色点心</li>
-                  <li>可口小菜</li>
-                </ul>
-            </div>
-            <div class="dBoxRight">
-              <p class="dBoxmeg"><strong>热销</strong><span>大家喜欢吃才是真的好吃</span></p>
-              <ul class="dbFoodsList">
-                <li>
-                  <img src="//fuss10.elemecdn.com/b/cd/8c85aaa7857b9b2c9e6884cfb14dbjpeg.jpeg?imageMogr/format/webp/thumbnail/!140x140r/gravity/Center/crop/140x140/" alt="">
-                  <dl class="Foods">
-                    <dt>
-                      网红蒸饺
-                    </dt>
-                    <dd class="textell">
-                      【5个一份】食材：小麦粉，猪肉，玉米，辅材。
-                    </dd>
-                    <dd>
-                      月售456份好评率97%
-                    </dd>
-                    <dd class="fdsprice">
-                      ￥5.8
-                    </dd>
-                    <span  class="addBlue">
-                    +
-                  </span>
-                  </dl>
-                  
-                </li>
-                <li>
-                  <img src="//fuss10.elemecdn.com/b/cd/8c85aaa7857b9b2c9e6884cfb14dbjpeg.jpeg?imageMogr/format/webp/thumbnail/!140x140r/gravity/Center/crop/140x140/" alt="">
-                  <dl class="Foods">
-                    <dt>
-                      网红蒸饺
-                    </dt>
-                    <dd class="textell">
-                      【5个一份】食材：小麦粉，猪肉，玉米，辅材。
-                    </dd>
-                    <dd>
-                      月售456份好评率97%
-                    </dd>
-                    <dd class="fdsprice">
-                      ￥5.8
-                    </dd>
-                    <span  class="addBlue">
-                    +
-                  </span>
-                  </dl>
-                  
-                </li>
-                <li>
-                  <img src="//fuss10.elemecdn.com/b/cd/8c85aaa7857b9b2c9e6884cfb14dbjpeg.jpeg?imageMogr/format/webp/thumbnail/!140x140r/gravity/Center/crop/140x140/" alt="">
-                  <dl class="Foods">
-                    <dt>
-                      网红蒸饺
-                    </dt>
-                    <dd class="textell">
-                      【5个一份】食材：小麦粉，猪肉，玉米，辅材。
-                    </dd>
-                    <dd>
-                      月售456份好评率97%
-                    </dd>
-                    <dd class="fdsprice">
-                      ￥5.8
-                    </dd>
-                    <span  class="addBlue">
-                    +
-                  </span>
-                  </dl>
-                  
-                </li>
-              </ul>
-            </div>
+        <div class="switchover" v-show="act==1">
+          <detailFoodList></detailFoodList>
+        </div>
+        <div class="discussBox" v-show="act==2">
+          <discuss></discuss>
         </div>
     </div>
 </template>
 
 <script>
-import {getStoreDetails} from '@/services/storedetails.js'
+import detailFoodList from './detailFoodList.vue'
+import discuss from './discuss.vue'
+// import {getStoreDetails} from '@/services/storedetails.js'
 export default {
+  data(){
+    return{
+      act:2
+    }
+  },
+  components:{
+    detailFoodList,
+    discuss
+  },
+  methods:{
+    Ordering(rest){
+      this.act = rest
+    }
+  },
   mounted(){
-    getStoreDetails().then(result=>{
-      console.log(result)
-    })
+    // getStoreDetails().then(result=>{
+    //   console.log(result)
+    // })
   }
 };
 </script>
 
-<style>
-.textell{
-  text-overflow: ellipsis;
-  overflow: hidden;
-  white-space: nowrap;
-  width:100%;
-}
+<style scoped>
 .godetail {
   width: 100%;
   position: absolute;
   top: 0;
+  bottom:0;
   z-index: 999;
   background: #fff;
   /* overflow: hidden; */
@@ -214,20 +105,7 @@ export default {
   color: #999;
 
 }
-.swiperList {
-  width: 100%;
-  overflow-x: hidden;
-}
-.swiperList li {
-  float: left;
-  width: 30%;
-  position: relative;
-  padding-left: 15px;
-}
-.swiperList li img {
-  width: 120px;
-  height: 120px;
-}
+
 .addBlue{
      position: absolute;
     bottom: 0;
@@ -247,6 +125,7 @@ export default {
   margin: 0 auto;
   background: #fff4f4;
   line-height: 0.25rem;
+  margin: 10px auto;
 }
 .adviaty i {
   font-size: 0.1rem;
@@ -280,85 +159,6 @@ export default {
   color: #000;
   border-bottom: 2px solid rgb(96, 182, 216);
 }
-.storeito {
-  color: #333;
-  font-size: 0.15667rem;
-  font-weight: 700;
-  line-height: 0.4rem;
-  margin-top: 5px;
-  text-indent: 0.1rem;
-}
-.fds1{
-    line-height: 0.28rem;
-}
-.monsale{
-  font-size:0.1rem;
-  color:#666;
-  margin-bottom:4px;
-}
-.price{
-    color: rgb(255, 83, 57);
-    font-weight: 700;
-    font-size: 0.16rem;
-}
-.detailBox{
-  margin-top:24px;
-}
-.dBoxLeft{
-  width:18%;
-  float:left;
-  background:#fafafa;
-}
-.dBoxLeft li{
-  text-align:center;
-  line-height:49px;
-  font-size:0.12rem;
-}
-.dBoxLeft li:nth-child(1){
-  background:#fff;
-}
-.dBoxRight{
-  width:82%;
-  box-sizing:border-box;
-  padding-left:10px;
-  float:left;
-}
-.dBoxmeg{
-  line-height:29px;
-}
-.dBoxmeg span{
-  font-size:0.1rem;
-  color:#666;
-  padding-left:5px;
-}
-.dbFoodsList li{
-  height:95px;
-  margin:10px 0;
-}
-.dbFoodsList li img{
-  width:95px;
-  height:95px;
-  float:left;
-}
-.Foods{
-  width:64%;
-  height:95px;
-  position: relative;
-  float:right;
-}
-.Foods dt{
-  font-weight: 700;
-}
-.Foods span{
-  right:10px;
-}
-.Foods dd{
-  font-size:0.1rem;
-  color:#999;
-}
-.Foods .fdsprice{
-  font-size:0.18rem;
-      line-height: 0.4rem;
-  color:rgb(255, 83, 57);
-}
+
+
 </style>
